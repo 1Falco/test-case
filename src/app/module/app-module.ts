2961e33988b928bcd02750e01aeb52aa0715c79b
lsokolski <lsokolski@whitlabelcoders.com>
2023-03-06 11:18:15 +0100

    Dev init

---
+++ src/app/module/app-module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { AppComponent } from "../components/app/app.component";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [CommonModule],
+})
+export class AppModule {}
